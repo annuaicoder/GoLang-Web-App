61ce55699ea3e1338d217621f0acedc43fa22983
Anas <153397401+annuaicoder@users.noreply.github.com>
2024-09-06 20:51:01 -0700

    Update Main.go

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "html/template"
+    "net/http"
 )
 
+var templates = template.Must(template.ParseFiles("templates/home.html", "templates/about.html"))
 
-func home(write http.ResponseWriter, r*http.Request) {
-	fmt.Fprintf(write,"Hello World!")
+func home(write http.ResponseWriter, r *http.Request) {
+    err := templates.ExecuteTemplate(write, "home.html", nil)
+    if err != nil {
+        http.Error(write, err.Error(), http.StatusInternalServerError)
+    }
 }
 
-func about(write http.ResponseWriter, r*http.Request) {
-	fmt.Fprintf(write,"About us!")
+func about(write http.ResponseWriter, r *http.Request) {
+    err := templates.ExecuteTemplate(write, "about.html", nil)
+    if err != nil {
+        http.Error(write, err.Error(), http.StatusInternalServerError)
+    }
 }
 
 func main() {
-	http.HandleFunc("/",home)
-	http.HandleFunc("/about",about)
-	http.ListenAndServe(":8080",nil)
+    http.HandleFunc("/", home)
+    http.HandleFunc("/about", about)
+    http.ListenAndServe(":8080", nil)
 }
